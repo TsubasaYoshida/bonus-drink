2f7e6aa170b69d7bbc61ebabcdfa558405d1075e
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-07-15 22:44:35 +0900

    解答完了

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,21 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+
+    quotient = amount / 3
+    remainder = amount % 3
+
+    total += quotient
+
+    while (quotient + remainder) / 3 > 0
+      tmp_q_r = quotient + remainder
+      quotient = tmp_q_r / 3
+      remainder = tmp_q_r % 3
+      total += quotient
+    end
+
+    total
   end
 end
+
+puts BonusDrink.total_count_for(100)
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify {expect(BonusDrink.total_count_for(0)).to eq 0}
+
+  it '1本購入の場合、トータル1本飲める' do
+    expect(BonusDrink.total_count_for(1)).to eq 1
+  end
+
+  it '3本購入の場合、トータル4本飲める' do
+    expect(BonusDrink.total_count_for(3)).to eq 4
+  end
+
+  it '11本購入の場合、トータル16本飲める' do
+    expect(BonusDrink.total_count_for(11)).to eq 16
+  end
+
+  it '100本購入の場合、トータル149本飲める' do
+    expect(BonusDrink.total_count_for(100)).to eq 149
+  end
+
 end
